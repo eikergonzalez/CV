96eb78cb86430af111518a08344791632267a1b0
root <root@sd-5188427-l.dattaweb.com>
2025-12-14 07:58:24 -0500

    react version

--- backend/sendMail.js
+++ backend/sendMail.js
@@ -45,4 +45,4 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-app.listen(5001, () => console.log('Servidor escuchando en puerto 5000'));
+app.listen(5001, () => console.log('Servidor escuchando en puerto 5001'));
